b28d68e0ede37e8c82dee961d86efc73d2b6d1dd
Sámuel Fekete <feketesamu@gmail.com>
2023-05-21 16:35:21 +0200

    test scaffold

--- test/lunchcodes.js
+++ test/lunchcodes.js
@@ -3,32 +3,49 @@ const LunchCodes = artifacts.require("LunchCodes");
 contract('LunchCodes', (accounts) => {
   let guard1 = accounts[0]
   let guard2 = accounts[1]
+  let janitor = accounts[2]
   it('Janitor enters the building then leaves', async () => {
     const lc = await LunchCodes.deployed();
 
-    await lc.requestEntry({from: accounts[2]})
+    await lc.requestEntry({from: janitor})
     await lc.approve({from: guard1})
     await lc.approve({from: guard2})
-    await lc.entry({from: accounts[2]})
+    await lc.entry({from: janitor})
 
     let ep = await lc.getExtraPerson()
-    assert.equal(ep, accounts[2])
+    assert.equal(ep, janitor)
     let actualLog = await lc.getLog()
     assert.equal(actualLog.length, 1)
-    assert.equal(actualLog[0].person, accounts[2])
+    assert.equal(actualLog[0].person, janitor)
     assert.equal(actualLog[0].out, false)
 
-    await lc.requestExit({from: accounts[2]})
+    await lc.requestExit({from: janitor})
     await lc.approve({from: guard1})
     await lc.approve({from: guard2})
-    await lc.exit({from: accounts[2]})
+    await lc.exit({from: janitor})
 
     ep = await lc.getExtraPerson()
     assert.equal(ep, 0)
     actualLog = await lc.getLog()
     assert.equal(actualLog.length, 2)
-    assert.equal(actualLog[1].person, accounts[2])
+    assert.equal(actualLog[1].person, janitor)
     assert.equal(actualLog[1].out, true)
 
   });
+
+  it('Shiftchange', async () => {
+
+  })
+
+  it('Prevents more than 3 people in the facility', async () => {
+
+  })
+
+  it('Prevents the guard from leaving the facility while on duty', async () => {
+
+  })
+
+  it('Someone tries to enter, the guards deny it. Janitor can still enter afterwards', async () => {
+
+  })
 });
